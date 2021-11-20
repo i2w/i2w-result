fe35ddb6da0e9cfc8ca5cf1d477d7049aea216d5
Ian White <ian.w.white@gmail.com>
2021-11-20 13:28:12 +1100

    Improve comments

--- lib/i2w/result/methods.rb
+++ lib/i2w/result/methods.rb
@@ -2,10 +2,12 @@
 
 module I2w
   module Result
+    # result methods, requires #success? to be defined
+    # if success, #value must be defined, otherwise #failure and #errors must be defined
     module Methods
       def failure? = !success?
 
-      # return the value if succes, or failure argument, or block called with failure
+      # return the value if succes, or failure argument, or block called with self
       def value_or(arg = nil, &block) = success? ? value : (block&.call(self) || arg)
 
       def deconstruct = success? ? [:success, value] : [:failure, failure, errors.details]
@@ -13,7 +15,7 @@ module I2w
       # if success, return the result of the block called with value, if failure return self
       def and_then = success? ? Result.to_result(yield(value)) : self
 
-      # if failure, return the result of the block called with failure and errors
+      # if failure, return the result of the block called with failure and errors, if success return self
       def or_else = success? ? self : Result.to_result(yield(failure, errors))
 
       # if success, call the block with the value, always return self
